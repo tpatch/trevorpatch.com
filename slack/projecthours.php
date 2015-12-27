154d75998a0129ef889df01a0c450dde7e47c2b3
Trevor Patch <trevor.patch@gmail.com>
2015-12-27 14:54:44 -0600

    New projecthours slack command

---
+++ slack/projecthours.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+
+REQUIREMENTS
+
+* A custom slash command on a Slack team
+* A web server running PHP5 with cURL enabled
+
+USAGE
+
+* Place this script on a server running PHP5 with cURL.
+* Set up a new custom slash command on your Slack team: 
+  http://my.slack.com/services/new/slash-commands
+* Under "Choose a command", enter whatever you want for 
+  the command. /isitup is easy to remember.
+* Under "URL", enter the URL for the script on your server.
+* Leave "Method" set to "Post".
+* Decide whether you want this command to show in the 
+  autocomplete list for slash commands.
+* If you do, enter a short description and usage hint.
+
+*/
+
+
+# Grab some of the values from the slash command, create vars for post back to Slack
+$command = $_POST['command'];
+$text = $_POST['text'];
+$token = $_POST['token'];
+$token = $_POST['channel_name'];
+
+# Check the token and make sure the request is from our team 
+if($token != '4P2J7rxDyWQ4by4OFrzNzwqe'){ #replace this with the token from your slash command configuration page
+  $msg = "The token for the slash command doesn't match. Check your script.";
+  die($msg);
+  echo $msg;
+}
+
+$reply = ":thumbsup: I am happy to report that *<http://".$response_array["domain"]."|".$response_array["domain"].">* is *up*!";
+
+# Send the reply back to the user. 
+echo $reply;
