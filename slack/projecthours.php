46db2b46840bdc29fa2d3ddb88977cf07eac5e30
Trevor Patch <trevor.patch@gmail.com>
2015-12-28 10:55:50 -0600

    Adding status command that returns total hours

--- slack/projecthours.php
+++ slack/projecthours.php
@@ -1,8 +1,5 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 $connect = mysqli_connect('localhost', 'trevorpatch.com', 'Y=Fx64#&kzs!g=AY', 'trevorpatchcom');
 if (!$connect) {
     $msg = "Script failed to connect to database";
@@ -19,13 +16,30 @@ $user = $_POST['user_name'];
 $time = rand(1, 12);
 
 # Check the token and make sure the request is from our team 
-if($token != '4P2J7rxDyWQ4by4OFrzNzwqe'){ #replace this with the token from your slash command configuration page
+if($token != '4P2J7rxDyWQ4by4OFrzNzwqe'){
   $msg = "The token for the slash command doesn't match. Check your script.";
   die($msg);
   echo $msg;
 }
 
 if ( strpos(trim($text), 'status') ) {
+    $monday = strtotime('last monday', strtotime('tomorrow'));
+    $sunday = strtotime('next sunday', strtotime('yesterday'));
+    $sql = "SELECT SUM(hours) AS TotalHours FROM hours WHERE Project = ? AND DateAdded >= ? AND DateAdded <= ? GROUP BY Project";
+    if ($stmt = mysqli_prepare($connect, $sql)) {
+        $stmt->bind_param("sss", $channelname, $monday, $sunday);
+        $stmt->execute();
+
+        $res = $stmt->get_result();
+        while($row = $res->fetch_array(MYSQLI_ASSOC)) {
+            $totalhours = $row;
+        }
+
+        $reply = ":clock". $time .": There are currently 8 hours on the ". $totalhours ." project this week.";
+    } else {
+        $reply = "Sorry, your hours couldn't be saved. Try again in a few moments.";
+    }
+
     $reply = ":clock". $time .": There are currently ". $text ." hours logged this week on this project.";
 } else if ( is_int(intval(trim($text))) ) {
     $text = intval(trim($text));
