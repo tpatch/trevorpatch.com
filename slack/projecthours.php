eb5c0be8116e4e148d962188f7732a7634a1f4f1
Trevor Patch <trevor.patch@gmail.com>
2015-12-28 09:25:25 -0600

    Script modifications, hooking up database

--- slack/projecthours.php
+++ slack/projecthours.php
@@ -1,33 +1,18 @@
 <?php
 
-/*
-
-REQUIREMENTS
-
-* A custom slash command on a Slack team
-* A web server running PHP5 with cURL enabled
-
-USAGE
-
-* Place this script on a server running PHP5 with cURL.
-* Set up a new custom slash command on your Slack team: 
-  http://my.slack.com/services/new/slash-commands
-* Under "Choose a command", enter whatever you want for 
-  the command. /isitup is easy to remember.
-* Under "URL", enter the URL for the script on your server.
-* Leave "Method" set to "Post".
-* Decide whether you want this command to show in the 
-  autocomplete list for slash commands.
-* If you do, enter a short description and usage hint.
-
-*/
-
+$connect = mysqli_connect('localhost', 'trevorpatch.com', 'Y=Fx64#&kzs!g=AY', 'trevorpatchcom');
+if (!$connect) {
+    $msg = "Script failed to connect to database";
+    die($msg);
+    echo $msg;
+}
 
 # Grab some of the values from the slash command, create vars for post back to Slack
 $command = $_POST['command'];
 $text = $_POST['text'];
 $token = $_POST['token'];
 $channelname = $_POST['channel_name'];
+$user = $_POST['user_name'];
 $time = rand(1, 12);
 
 # Check the token and make sure the request is from our team 
@@ -37,7 +22,23 @@ if($token != '4P2J7rxDyWQ4by4OFrzNzwqe'){ #replace this with the token from your
   echo $msg;
 }
 
-$reply = ":clock". $time .": Thanks, we have you down for ". $text ." hours!";
+if ( strpos($text, 'status') ) {
+    $reply = ":clock". $time .": There are currently ". $text ." hours logged this week on this project.";
+} else if ( is_int($text) ) {
+    $sql = "INSERT INTO hours VALUES (NULL, ?, ?, ?, NOW())";
+    if ($stmt = mysqli_prepare($connect, $sql)) {
+        $stmt->bind_param("sss", $channelname, $user, $text);
+        $stmt->execute();
+
+        $reply = ":clock". $time .": Thanks, we have you down for ". $text ." hours!";
+    } else {
+        $reply = "Sorry, your hours couldn't be saved. Try again in a few moments.";
+    }
+
+    mysqli_close($connect);
+} else {
+    $reply = "Sorry, that's not a valid command.";
+}
 
 # Send the reply back to the user. 
 echo $reply;
