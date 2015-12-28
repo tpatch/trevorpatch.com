8dc6361dc958db0af5425c640ebd05766be92151
Trevor Patch <trevor.patch@gmail.com>
2015-12-28 14:46:12 -0600

    Testing

--- slack/test.php
+++ slack/test.php
@@ -1,6 +1,14 @@
 <?php
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
+
+    $connect = mysqli_connect('localhost', 'trevorpatch.com', 'Y=Fx64#&kzs!g=AY', 'trevorpatchcom');
+    if (!$connect) {
+        $msg = "Script failed to connect to database";
+        die($msg);
+        echo $msg;
+    }
+
     $monday = strtotime('last monday', strtotime('tomorrow'));
     $sunday = strtotime('next sunday', strtotime('yesterday'));
     $sql = "SELECT SUM(hours) AS TotalHours FROM hours WHERE Project = ? AND DateAdded >= ? AND DateAdded <= ? GROUP BY Project";
